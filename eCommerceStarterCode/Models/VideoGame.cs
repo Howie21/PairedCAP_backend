df5d0657af159dbc8655fbd79428fac47074b99f
Howie21 <bryce9643@gmail.com>
2021-12-06 17:55:43 -0500

    Creation of the VideoGames Model and Migrated

---
+++ eCommerceStarterCode/Models/VideoGame.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eCommerceStarterCode.Models
+{
+    public class VideoGame
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+        public string System { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+
+
+    }
+}
